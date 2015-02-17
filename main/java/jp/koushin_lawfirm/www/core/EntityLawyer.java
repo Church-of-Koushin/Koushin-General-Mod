b4a8f14e96a325d4af4ff6796bd81ab3b5265eb6
Church-of-Koushin <f8308@trbvm.com>
2015-02-17 18:49:19 +0900

    Lawyerのドロップアイテムを作った。一応豚肉にしといた。

--- main/java/jp/koushin_lawfirm/www/core/EntityLawyer.java
+++ main/java/jp/koushin_lawfirm/www/core/EntityLawyer.java
@@ -2,10 +2,21 @@ package jp.koushin_lawfirm.www.core;
 
 import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.monster.EntityZombie;
+import net.minecraft.init.Items;
 import net.minecraft.world.World;
 
 public class EntityLawyer extends EntityMob {
 	public EntityLawyer(World world) {
 		super(world);
+	}
+
+	@Override
+	public void dropFewItems(boolean doesPlayerKill, int level) {
+		int j = this.rand.nextInt(3) + 1 + this.rand.nextInt(1 + level);
+
+		for (int k = 0; k < j; ++k) {
+
+			this.dropItem(Items.porkchop, 1);
+		}
 	}
 }
